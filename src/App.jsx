0603426edc0963f39712b62a0c31aeaf215ac145
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-29 12:40:30 +0545

    youtube video card completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import VideoCard from "./components/Videocard"
 export default function App(){
   return(<>
-    <VideoCard></VideoCard>
+    <VideoCard
+    title={"Man-[Na] - Kaalpanik/ Maayajastai Offical Video"}
+    thumbnail={"thumbnail.png"}
+    logo={"logo.png"}
+    aurthor={"Bartika Ema Rai"}
+    views={"13M"}
+    timestamp={"3 years"}
+    ></VideoCard>
   </>)
 } 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function VideoCard(){
+export default function VideoCard(props){
     return <div className='w-[90%]'>
-        <img src="./thumbnail.png" alt="" className='w-full rounded-2xl' />
+        <img src={props.thumbnail} alt="" className='w-full rounded-2xl' />
         <div className="flex mt-3">
-            <img src="./logo.png" alt="" className='rounded-full w-15 h-15'/>
+            <img src={props.logo} alt="" className='rounded-full w-15 h-15'/>
             <div className='pl-3'>
-                <h1 className='text-xl'>Man-[Na] - Kaalpanik/ Maayajastai Offical Video</h1>
-                <p className='text-gray-600'>Bartika Eam Rai</p>
-                <p className='text-gray-600'>13M views | 3 years agao</p>
+                <h1 className='text-xl'>{props.title}</h1>
+                <p className='text-gray-600'>{props.aurthor}</p>
+                <p className='text-gray-600'>{props.views} views |  {props.timestamp} ago</p>
             </div>
         </div>
     </div>
