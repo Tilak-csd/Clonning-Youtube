e51068ffd93988f191004484472e4f0d9ea0ab91
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-29 21:28:44 +0545

    adding the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import Notification from './Notification'
+import Menu from './Menu'
 
 
 export default function Navbar() {
     return (
         <div className="flex justify-around items-center h-15">
-            <div>
-               Youtube
+            <div className='flex gap-3'>
+            <Menu />
+            <img src="./youtubelogo.png" alt="" className='w-auto h-6' />
             </div>
             <div>
                 <SearchBar />
             </div>
-            <div>
-                Login
+            <div className='flex gap-3'>
+                <Notification></Notification>
+                <img src="./userlogo.png" className='w-9 h-9 rounded-full'/>
             </div>
         </div>
     )
