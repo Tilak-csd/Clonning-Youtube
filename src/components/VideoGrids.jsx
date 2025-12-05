9150e0579b877c963cce892fd7734d019b761995
Tilak-csd <tilakgubhaju3@gmail.com>
2025-12-05 23:11:25 +0545

    adding the pagination feature

--- src/components/VideoGrids.jsx
+++ src/components/VideoGrids.jsx
@@ -3,23 +3,35 @@ const VideoCard = React.lazy(() => import('./Videocard'))
 import { data } from '../data'
 
 export default function VideoGrids() {
-   const [vissblecount, setVissiblecount] = useState(6)
-   const loadMore = ()=>{
-    setVissiblecount(prev => prev + 6)
-   }
+    // loading more video when scrolled down to infinity
+    //    const [vissblecount, setVissiblecount] = useState(6)
+    //    const loadMore = ()=>{
+    //     setVissiblecount(prev => prev + 6)
+    //    }
 
-   useEffect(()=>{
-    const handleScroll = ()=>{
-        if(window.innerHeight+ window.scrollY >= document.body.offsetHeight - 100){
-            loadMore()
-        }
-    }
-    window.addEventListener("scroll", handleScroll)
+    //    useEffect(()=>{
+    //     const handleScroll = ()=>{
+    //         if(window.innerHeight+ window.scrollY >= document.body.offsetHeight - 100){
+    //             loadMore()
+    //         }
+    //     }
+    //     window.addEventListener("scroll", handleScroll)
 
-    return ()=> window.removeEventListener('scroll', handleScroll)
-   }, [])
+    //     return ()=> window.removeEventListener('scroll', handleScroll)
+    //    }, [])
+
+    const [currentpage, setCurrentpage] = useState(1)
+    const videoperpage = 6
+
+    const indexofLastVideo = currentpage * videoperpage
+    const indexofFirstVideo = indexofLastVideo - videoperpage
+    const currentVideos = data.slice(indexofFirstVideo, indexofLastVideo)
+
+  
+
     return (<div className='w-full justify-items-center content-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-        {data.slice(0, vissblecount).map((data, idx) => {
+        {/* {data.slice(0, vissblecount).map((data, idx) => { */}
+        {currentVideos.map((data, idx) => {
             return <Suspense fallback={<p>Loading.....</p>} key={idx}>
 
                 <VideoCard title={data.title}
@@ -31,5 +43,15 @@ export default function VideoGrids() {
                 />
             </Suspense>
         })}
+
+        <Pagination setCurrentpage={setCurrentpage} currentpage={currentpage} indexofLastVideo={indexofFirstVideo} data={data}/>
     </div>)
-} 
+}
+
+// pagination
+  function Pagination({ currentpage, setCurrentpage, indexofLastVideo, data }) {
+        return <div className='w-full flex items-center justify-center gap-3'>
+            <button disabled={currentpage === 1} onClick={() => setCurrentpage(prev => prev - 1)}>Prev</button>
+            <button disabled={indexofLastVideo >= data.length} onClick={() => setCurrentpage(prev => prev + 1)}>Next</button>
+        </div>
+    }
